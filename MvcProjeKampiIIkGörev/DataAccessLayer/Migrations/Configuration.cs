b3b3f1daa061e101a81df51039a03e1e0120265d
ahmetcinaer <44680374+ahmetcinaer@users.noreply.github.com>
2021-07-31 23:10:09 +0300

    Mvc Proje Kampi

---
+++ MvcProjeKampiIIkGörev/DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    //Projede değişiklik yaptım ve bunu veritabanına yansıtmak istiyorsam migration yapmam lazım C# ile Sql arasında köprü
+    internal sealed class Configuration : DbMigrationsConfiguration<DataAccessLayer.Concrete.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DataAccessLayer.Concrete.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
